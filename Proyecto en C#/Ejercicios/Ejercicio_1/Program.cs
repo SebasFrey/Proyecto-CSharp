00727655c229f9a1d266fde0acaf0be5416da252
SebasFrey <lomasshidokis@gmail.com>
2024-11-14 14:37:15 -0500

    Add files via upload

---
+++ Proyecto en C#/Ejercicios/Ejercicio_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+
+      Console.WriteLine("Ingresa tu nombre: ");
+
+      string nombre = Console.ReadLine();
+
+      Console.WriteLine("Hola " + nombre + ". Te doy la bienvenida a este programa el dia de hoy " + DateTime.Now + " La frase del dia es:\n Sabias que si puedes imaginarlo, puedes programarlo.");
+
+
+
+
+
+
+
+    }
+  }
+
+}
