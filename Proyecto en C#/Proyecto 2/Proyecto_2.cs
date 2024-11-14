861e0f42cae035a7b04a101ba42264d3995f5dd0
SebasFrey <lomasshidokis@gmail.com>
2024-11-14 10:21:48 -0500

    Add files via upload

---
+++ Proyecto en C#/Proyecto 2/Proyecto_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpProyecto_2
+{
+  class Proyecto_2
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hola Mundo.\nSi puedes imaginarlo, puedes programarlo.");
+
+
+
+
+
+    }
+  }
+}
