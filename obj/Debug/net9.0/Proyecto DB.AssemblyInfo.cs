1b815e2574f86edc640d3bf2b44f21d3c0520b2b
Sebastian Freyles <freylessebastian@gmai.com>
2024-11-29 11:21:01 -0500

    Actualización 1.23 - Corrección en el manejo de archivos y estructura de directorios.

--- obj/Debug/net9.0/Proyecto DB.AssemblyInfo.cs
+++ obj/Debug/net9.0/Proyecto DB.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Base d Datos")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto DB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+388b90d21cb61175ff43c35cc17f1a37e66064fd")]
-[assembly: System.Reflection.AssemblyProductAttribute("Base d Datos")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Base d Datos")]
+[assembly: System.Reflection.AssemblyProductAttribute("Proyecto DB")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Proyecto DB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
